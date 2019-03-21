694ecb08fd671d427bee1d73fd28cce1cc3ba956
etigerstudio <etigerstudio@gmail.com>
2019-03-21 14:37:36 +0800

    impl: optimize css when build.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
 const { src, dest } = require('gulp');
 const sass = require('gulp-sass');
+const cleanCSS = require('gulp-clean-css');
 
 function build() {
     init();
     return src('src/*')
         .pipe(sass().on('error', sass.logError))
+        .pipe(cleanCSS({
+            format: 'beautify',
+            level: 2
+        }))
         .pipe(dest('dist/'));
 }
 
